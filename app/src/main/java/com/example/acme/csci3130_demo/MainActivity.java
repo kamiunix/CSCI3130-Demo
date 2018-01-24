c699ed801252f577fdf001407a37edaf559f017a
kamiunix <sam.c.tur@gmail.com>
2018-01-24 09:30:01 -0400

    Demo Changes

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.acme.csci3130_demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button but;
+    EditText etxt;
+    TextView vtxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        but = (Button) findViewById(R.id.button);
+        etxt = (EditText) findViewById(R.id.editText);
+        vtxt = (TextView) findViewById(R.id.textView);
+
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = etxt.getText().toString();
+                vtxt.setText(msg);
+            }
+        });
     }
 }
